10e2aeaaf51bdc676ef1fb65641816f01def8609
Daniel Marner <danielmarner@Daniels-Air.home>
2017-03-23 23:01:23 +0100

    + location-tracking *works* (tested) + distance_m + duration

--- js/run.js
+++ js/run.js
@@ -1,79 +1,71 @@
 var Observable = require("FuseJS/Observable");
 var GeoLocation = require("FuseJS/GeoLocation");
 var Timer = require("FuseJS/Timer");
-
-var lastcoordinates = {lat : 0.0, lon : 0.0};
-
 var distance_m = Observable(0);
 var totalSeconds = Observable(0);
-
 var run_active = true;
-
- // Immediate
- var immediateLocation = JSON.stringify(GeoLocation.location);
+var lastLocation = {lat : 0.0, lon:0.0};
 
 
- // Timeout
- var timeoutLocation = Observable("");
- var timeoutMs = 5000;
+   // Immediate
+   var immediateLocation = JSON.stringify(GeoLocation.location);
+
+
+ var running_timer =   Timer.create(function() {
+
+if(run_active)
+totalSeconds.value += 1;
+
+     GeoLocation.getLocation(3000).then(function(location) {
+
+if(lastLocation.lat > 0)
+{
+  distance_m.value += (calcCrow(lastLocation.lat, lastLocation.lon, location.latitude, location.longitude)*1000);
+  console.log(distance_m);
+}
+
+lastLocation.lat = location.latitude;
+lastLocation.lon = location.longitude;
+
+
+     }).catch(function(fail) {
+         console.log("getLocation fail " + fail);
+     });
+
+
+   }, 1000,true);
 
 
 
- Timer.create(function() {
 
-   console.log(totalSeconds.value);
-
-   var parsedLocation = JSON.parse(immediateLocation);
-   lastcoordinates.lat = parsedLocation.latitude;
-   lastcoordinates.lon =  parsedLocation.longitude;
-
-   if(lastcoordinates.lat >0)
-   {
-   distance_m.value += calcCrow(lastcoordinates.lat, lastcoordinates.lon, parsedLocation.latitude, parsedLocation.longitude)*1000;
-   console.log("Distance: "+distance_m.value);
-   console.log("lat: "+lastcoordinates.lat);
-   console.log("lon: "+lastcoordinates.lon);
-    }
+   // Timeout
+   var timeoutLocation = Observable("");
+   var timeoutMs = 1000;
 
 
-   totalSeconds.value += 1;
+   // Continuous
+   var continuousLocation = GeoLocation.observe("changed").map(JSON.stringify);
 
-   }, 1000, true);
+   function startContinuousListener() {
+       var intervalMs = 1000;
+       var desiredAccuracyInMeters = 10;
+       GeoLocation.startListening(intervalMs, desiredAccuracyInMeters);
 
+   }
 
+   function stopContinuousListener() {
+       GeoLocation.stopListening();
+   }
 
- GeoLocation.getLocation(timeoutMs).then(function(location) {
-
-   console.log("Location updated");
-
-
-     timeoutLocation.value = JSON.stringify(location);
-
- }).catch(function(fail) {
-     console.log("getLocation fail " + fail);
- });
-
- // Continuous
- var continuousLocation = GeoLocation.observe("changed").map(JSON.stringify);
-
- function startContinuousListener() {
-     var intervalMs = 1000;
-     var desiredAccuracyInMeters = 10;
-     GeoLocation.startListening(intervalMs, desiredAccuracyInMeters);
- }
-
- function stopContinuousListener() {
-     GeoLocation.stopListening();
- }
 
  var finishrun = function()
  {
        stopContinuousListener();
+       running_timer.stop();
        run_active = false;
        router.goto("dashboard");
  }
 
- startContinuousListener();
 
  //This function takes in latitude and longitude of two location and returns the distance between them as the crow flies (in km)
    function calcCrow(lat1, lon1, lat2, lon2)
